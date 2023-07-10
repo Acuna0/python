65a89dac397558fc397d2249d2a7ba0122dcf65e
Ryan <139078564+Acuna0@users.noreply.github.com>
2023-07-09 22:54:53 -0500

    Added extra unit tests to account for more situations.

--- test_account.py
+++ test_account.py
@@ -12,46 +12,58 @@ class Test:
         del self.person2
 
     def test_init(self):
-        # Testing
+        # Testing default values
         assert self.person1.get_name() == "Ryan"
         assert self.person2.get_name() == "Molly"
-        assert self.person1.get_balance() == 0
-        assert self.person2.get_balance() == 0
+        assert self.person1.get_balance() == pytest.approx(0)
+        assert self.person2.get_balance() == pytest.approx(0)
 
     def test_deposit(self):
         # Testing whether deposit occurred (True = yes, False = no)
         assert self.person1.deposit(-500) is False
+        assert self.person1.get_balance() == pytest.approx(0)
         assert self.person2.deposit(0) is False
+        assert self.person2.get_balance() == pytest.approx(0)
 
         assert self.person1.deposit(100) is True
+        assert self.person1.get_balance() == pytest.approx(100)
         assert self.person2.deposit(25.50) is True
+        assert self.person2.get_balance() == pytest.approx(25.50)
 
     def test_withdraw(self):
         # Testing whether withdraw occurred (True = yes, False = no)
         assert self.person1.withdraw(-500) is False
+        assert self.person1.get_balance() == pytest.approx(0)
         assert self.person2.withdraw(0) is False
+        assert self.person2.get_balance() == pytest.approx(0)
 
         # Testing acceptable withdrawal ranges
         self.person1.deposit(100)
+        assert self.person1.get_balance() == pytest.approx(100)
         assert self.person1.withdraw(100.01) is False
+        assert self.person1.get_balance() == pytest.approx(100)
         assert self.person1.withdraw(-10) is False
-        assert self.person1.withdraw(100) is True
+        assert self.person1.get_balance() == pytest.approx(100)
+        assert self.person1.withdraw(50) is True
+        assert self.person1.get_balance() == pytest.approx(50)
 
         self.person2.deposit(55.25)
         assert self.person2.withdraw(0) is False
+        assert self.person2.get_balance() == pytest.approx(55.25)
         assert self.person2.withdraw(55.24) is True
+        assert self.person2.get_balance() == pytest.approx(0.01)
 
     def test_get_balance(self):
         # Testing initial balances
-        assert self.person1.get_balance() == 0
-        assert self.person2.get_balance() == 0
+        assert self.person1.get_balance() == pytest.approx(0)
+        assert self.person2.get_balance() == pytest.approx(0)
 
         # Testing balances after deposit
         self.person1.deposit(10)
-        assert self.person1.get_balance() == 10
+        assert self.person1.get_balance() == pytest.approx(10)
 
         self.person2.deposit(-500)
-        assert self.person2.get_balance() == 0
+        assert self.person2.get_balance() == pytest.approx(0)
 
     def test_get_name(self):
         # Testing names on initial object creation
